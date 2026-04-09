9e7e182da6d548c6ef379b2afbb87f3343a6f4b5
Syeda Maheen Kazmi <maheenkazmi172@gmail.com>
2026-04-09 22:33:08 +0500

    created sql tables

--- sqlfiles/table_creation.sql
+++ sqlfiles/table_creation.sql
@@ -1,90 +1,84 @@
---creating enum types for future use
-create type role_type as enum('admin', 'manager', 'operator');
+--creating enum types for later use
+create type role_type as enum ('admin', 'manager', 'operator');
 create type department_type as enum ('logistics', 'security_patrol');
-create type scheduled_status_type as enum('scheduled', 'in_progress', 'done');
-create type assignment_status_type as enum('active', 'completed');
+create type scheduled_status_type as enum ('scheduled', 'in_progress', 'done');
+create type assignment_status_type as enum ('active', 'completed');
 
 create table users(
 	user_id serial primary key,
 	name VARCHAR(50) not null,
-	email VARCHAR(50) not null unique, 
-		constraint email_format check (email like '%_@_%.__%');
+	email VARCHAR(100) not null unique, 
+		constraint email_format check (email like '%_@_%.__%'),
 	joining_date date not null default current_date,
-	role role_type not null, 
+	role role_type not null,
 	is_active boolean not null default true
 );
 
-create table security_admins(
-	admin_id varchar(20) primary key, --check it's datatype
+create table security_admin(
+	admin_id varchar(20) primary key,
 	user_id int not null unique,
-	username VARCHAR(25) not null unique,--assigned in backend
-	foreign key (user_id) references user(user_id)
+	username VARCHAR(25) not null unique,--check this on how to assign this, and whether it should be added here or in separate table
+	foreign key (user_id) references users(user_id)
 );
-
 create table fleet_manager(
-	manager_id text primary key unique,
-	user_id int,
-	username VARCHAR(25),--check this on how to assign this, and whether it should be added here or in separate table
-	foreign key (user_id) references user(user_id),
-	department enum --check this out too
+	manager_id varchar(20) primary key,
+	user_id int not null unique,
+	username VARCHAR(25) not null unique,--check this on how to assign this, and whether it should be added here or in separate table
+	foreign key (user_id) references users(user_id),
+	department department_type not null
 );
-
 create table operators(
-	op_id text primary key unique,
-	user_id int,
-	username VARCHAR(25),--check this on how to assign this, and whether it should be added here or in separate table
-	foreign key (user_id) references user(user_id),
-	manager_id text,
-	foreign key (manager_id) references fleet_managers (manager_id),
-	active status boolean
+	op_id varchar(20) primary key,
+	user_id int not null unique,
+	username VARCHAR(25) not null unique,--check this on how to assign this, and whether it should be added here or in separate table
+	foreign key (user_id) references users(user_id),
+	manager_id varchar(20) not null,
+	foreign key (manager_id) references fleet_manager(manager_id),
+	active_status boolean not null default false
 );
-
 create table passwords(
 	username VARCHAR(25) primary key,
-	pass_hash varchar(30),
-	user_id int,
-	foreign key (user_id) references user(user_id)
+	pass_hash varchar(255) not null,
+	user_id int not null unique,
+	foreign key (user_id) references users(user_id)
 );
-
 create table zones(
-	zone_id int primary key unique,
-	zone_name text,
-	boundary GEOMETRY(POLYGON, 4326),
-	is_forbidden boolean,
-	created_by text,
+	zone_id serial primary key,
+	zone_name varchar(100) not null,
+	boundary GEOMETRY(POLYGON, 4326) not null,
+	is_forbidden boolean not null default false,
+	created_by varchar(20) not null,
 	foreign key (created_by) references security_admin(admin_id)
 );
-
 create table asset(
-	asset_id int primary key unique,
-	asset_name text,
-	plate_number text,
-	scheduled_status enum --check this out too
+	asset_id serial primary key,
+	asset_name varchar(50) not null,
+	plate_number varchar(20) not null unique,
+	scheduled_status scheduled_status_type not null default 'scheduled'
 );
-
 create table location_logs(
 	log_id serial primary key,
-	asset_id int,
-	op_id text,
-	current_location GEOMETRY(POLYGON, 4326),
-	time_stamp TIMESTAMP,
-	foreign key (asset_id) references asset(asset_id),
-	foreign key (op_id) references operators(op_id)
+    asset_id int not null,
+    op_id varchar(20) not null,
+    current_location geometry(point, 4326) not null,
+    time_stamp timestamp not null default current_timestamp,
+    foreign key (asset_id) references assets(asset_id),
+    foreign key (op_id) references operators(op_id)
 );
-
-create table assignment(
+create table assignments(
 	assignment_id serial primary key,
-	manager_id text,
-	op_id text,
-	asset_id int,
-	foreign key (asset_id) references asset(asset_id),
-	foreign key (op_id) references operators(op_id),
-	foreign key (manager_id) references fleet_managers (manager_id),
-	assigned_at timestamp,
-	status enum --active or completed
+    manager_id varchar(20) not null,
+    op_id varchar(20) not null,
+    asset_id int not null,
+    assigned_at timestamp not null default current_timestamp,
+    status assignment_status_type not null default 'active',
+    foreign key (manager_id) references fleet_manager(manager_id),
+    foreign key (op_id) references operators(op_id),
+    foreign key (asset_id) references asset(asset_id),
+    constraint one_active_per_asset unique (asset_id, status)
 );
 create table dummy(
 	dummy_id serial primary key,
-	asset_name_fake VARCHAR(20),
-	location_fake VARCHAR(50)
+	asset_name_fake VARCHAR(50),
+	location_fake VARCHAR(100)
 );
